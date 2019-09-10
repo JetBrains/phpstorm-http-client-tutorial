f80956ffdd64fd8ca220e27c1175bacf7c7ae9c5
Roman Pronskiy <roman@pronskiy.com>
2019-09-10 18:13:41 +0300

    Init

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+$app->run();
